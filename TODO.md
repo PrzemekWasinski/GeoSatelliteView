a439ee4405a141c9872bcbe849d6c5d5a178a3ba
PrzemekWasinski <przemek282005@outlook.com>
2026-06-29 11:41:47 +0100

    Added custom output path

--- TODO.md
+++ TODO.md
@@ -1,6 +1,3 @@
 ### To Do:
 - Add more example outputs in `README.md`
-- Clear old images script
-
-### Optional:
-- Add GUI 
+- Add optional image keeping
--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #Bash script to compile this program
 
-cmake -S . -B build && cmake --build build -j$(nproc) && echo echo "Compiled successfully! Run with: ./build/main"
+cmake -S . -B build && cmake --build build -j$(nproc) && echo echo "Compiled successfully! Run with: ./run.sh"
